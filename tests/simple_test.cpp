52d331843a305d92c4326c1cd1029c8d2bcc2afd
Yuki Koyama <yuki@koyama.xyz>
2019-02-20 19:26:33 +0900

    Update test program

--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -5,6 +5,11 @@ int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
     visopt::SlidersWidget sliders_widget;
+    sliders_widget.setMinimumValue(0.0);
+    sliders_widget.setMaximumValue(1.0);
+    sliders_widget.setLowerBound(Eigen::Vector3d(- 1.0, - 1.0, - 1.0));
+    sliders_widget.setUpperBound(Eigen::Vector3d(+ 1.0, + 1.0, + 1.0));
+    sliders_widget.setArgument(Eigen::Vector3d(0.4, 0.2, 0.6));
     sliders_widget.setTargetFunction([](const Eigen::VectorXd& x)
                                      {
                                          return x.norm();
