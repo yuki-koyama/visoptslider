90b757b177dca317b50c2bbe4753c7ec438123c6
Yuki Koyama <yuki@koyama.xyz>
2019-02-20 11:59:17 +0900

    Add temporary sliders

--- src/visoptslider.cpp
+++ src/visoptslider.cpp
@@ -1,11 +1,19 @@
 #include <visoptslider/visoptslider.hpp>
+#include <QSlider>
+#include <QBoxLayout>
 
 namespace visopt
 {
     SlidersWidget::SlidersWidget(QWidget* parent)
     : QGroupBox(parent)
     {
+        QBoxLayout *sliders_layout = new QBoxLayout(QBoxLayout::TopToBottom);
 
+        sliders_layout->addWidget(new QSlider(Qt::Horizontal));
+        sliders_layout->addWidget(new QSlider(Qt::Horizontal));
+        sliders_layout->addWidget(new QSlider(Qt::Horizontal));
+
+        setLayout(sliders_layout);
     }
 
     namespace internal
