06fb9aedf967db77918867dfa34d782827a0dbdf
Yuki Koyama <yuki@koyama.xyz>
2019-07-10 23:07:16 +0900

    Add minimal class definitions

--- visoptslider/visoptslider.py
+++ visoptslider/visoptslider.py
@@ -1,9 +1,17 @@
-from PySide2.QtWidgets import QApplication, QWidget
+from PySide2.QtWidgets import QApplication, QWidget, QGroupBox
+
+class SliderWidget(QGroupBox):
+    def __init__(self, parent):
+        QGroupBox.__init__(self, parent)
+
+class VisualizationWidget(QWidget):
+    def __init__(self, parent):
+        QWidget.__init__(self, parent)
 
 if __name__ == "__main__":
     import sys
 
     app = QApplication()
-    widget = QWidget(None)
+    widget = SliderWidget(None)
     widget.show()
     sys.exit(app.exec_())
