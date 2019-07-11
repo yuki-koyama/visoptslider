62481d2b39bc3c513dfd17d37741ec026440091f
Yuki Koyama <yuki@koyama.xyz>
2019-07-12 08:33:53 +0900

    Add more info

--- setup.py
+++ setup.py
@@ -14,8 +14,14 @@ setuptools.setup(
     url="https://github.com/yuki-koyama/visoptslider",
     packages=setuptools.find_packages(),
     classifiers=[
+        "Development Status :: 3 - Alpha",
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        "matplotlib",
+        "numpy",
+        "PySide2",
     ],
 )
